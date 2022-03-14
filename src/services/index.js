b59b32b545f4090cd9c5d905b980684f7b944f7c
Krzysztof Grudziński <95306698+kgrudzinski@users.noreply.github.com>
2022-03-14 20:00:41 +0100

    feat: add services folder
    Services implement communication with backend.

---
+++ src/services/index.js
@@ -0,0 +1,35 @@
+import { invoke } from "@tauri-apps/api/tauri";
+
+let query = (name, args) => {
+  return invoke(name, args)
+    .then((data) => {
+      return new Promise(function (resolve, _reject) {
+        resolve(data);
+      });
+    })
+    .catch((err) => {
+      throw err;
+    });
+};
+
+let createService = (name) => {
+  return {
+    get: function () {
+      return query(`get_${name}_list`);
+    },
+    add: function (item) {
+      return query(`add_${name}`, item);
+    },
+    update: function (item) {
+      return query(`update_${name}`, item);
+    },
+    delete: function (id) {
+      return query(`delete_${name}`, id);
+    },
+  };
+};
+
+export const ProjectsService = createService("project");
+export const CategoriesService = createService("category");
+export const TasksService = createService("task");
+export const EntriesService = createService("entry");
