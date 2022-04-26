dbfbc32203e9f7b0919f05015397d06121a402e9
Krzysztof Grudziński <95306698+kgrudzinski@users.noreply.github.com>
2022-04-26 18:04:13 +0200

    feat: use ProjectForm component

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,8 +1,7 @@
 import { useState } from "react";
 import { useQuery, useMutation, useQueryClient } from "react-query";
 import { ProjectsService } from "services";
-import { Form } from "components/shared";
-import { ButtonColor } from "components/shared";
+import { ProjectForm } from "components/forms";
 
 const NEW_TASK = {
   id: 0,
@@ -106,17 +105,19 @@ export function Projects() {
           </button>
         </>
       ) : (
-        <ProjectForm
-          project={selected}
-          onClose={(item) => {
-            if (item.id === 0) {
-              add_project_mut.mutate(item);
-            } else {
-              update_project_mut.mutate(item);
-            }
-          }}
-          onCancel={() => setMode(Mode.VIEW)}
-        />
+        <div className="box">
+          <ProjectForm
+            project={selected}
+            onClose={(item) => {
+              if (item.id === 0) {
+                add_project_mut.mutate(item);
+              } else {
+                update_project_mut.mutate(item);
+              }
+            }}
+            onCancel={() => setMode(Mode.VIEW)}
+          />
+        </div>
       )}
       {error && (
         <div className="notification is-danger">
@@ -161,101 +162,3 @@ function ProjectList({ data, onEdit, onDelete }) {
     </table>
   );
 }
-
-function ProjectForm({ project, onClose, onCancel }) {
-  return (
-    <div className="box">
-      <Form initialData={project} onSubmit={onClose}>
-        <Form.Field>
-          <Form.Label>Name</Form.Label>
-          <Form.Input placeholder="Project name" name="name"></Form.Input>
-        </Form.Field>
-        <Form.Field>
-          <Form.Label>Description</Form.Label>
-          <Form.Textarea
-            placeholder="Description"
-            name="description"
-          ></Form.Textarea>
-        </Form.Field>
-        <Form.FieldGroup>
-          <Form.Button submit color={ButtonColor.LINK}>
-            OK
-          </Form.Button>
-          <Form.Button color={ButtonColor.LINK_LIGHT} onClick={onCancel}>
-            Cancel
-          </Form.Button>
-        </Form.FieldGroup>
-      </Form>
-    </div>
-  );
-}
-/*
-function ProjectSettings({ project, onClose, onCancel }) {
-  const [data, setData] = useState(project);
-
-  const onChange = (evt) => {
-    setData({ ...data, [evt.target.name]: evt.target.value });
-  };
-
-  return (
-    <div className="box">
-      <div className="field">
-        <label className="label">Name</label>
-        <div className="control">
-          <input
-            className="input"
-            type="text"
-            placeholder="Project name"
-            value={data.name}
-            name="name"
-            onChange={onChange}
-          />
-        </div>
-      </div>
-      <div className="field">
-        <label className="label">Description</label>
-        <div className="control">
-          <textarea
-            className="textarea"
-            type="text"
-            placeholder="Description"
-            value={data.description}
-            name="description"
-            onChange={onChange}
-          />
-        </div>
-      </div>
-      <div>
-        <label className="label">Categories</label>
-        <div className="columns">
-          <div className="column">
-            <div className="select">
-              <select size="8"></select>
-            </div>
-          </div>
-          <div className="column">Kol 2</div>
-          <div className="column">
-            <div className="control">
-              <div className="is-multiple select">
-                <select size="8"></select>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="field is-grouped">
-        <div className="control">
-          <button className="button is-link" onClick={() => onClose(data)}>
-            Ok
-          </button>
-        </div>
-        <div className="control">
-          <button className="button is-link is-light" onClick={onCancel}>
-            Cancel
-          </button>
-        </div>
-      </div>
-    </div>
-  );
-}
-*/
