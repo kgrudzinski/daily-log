9ede30335da4fc6461e9580149e36cb948b5d7ca
Krzysztof Grudziński <95306698+kgrudzinski@users.noreply.github.com>
2022-05-01 18:14:28 +0200

    fix: Correct layout and props of category form

--- src/components/forms/CategoryForm.jsx
+++ src/components/forms/CategoryForm.jsx
@@ -2,19 +2,19 @@ import { Form, ButtonColor } from "components/shared";
 
 export function CategoryForm({ data, onClose, onCancel }) {
   return (
-    <Form>
-      <Form.Field initialData={data} onSubmit={onClose}>
+    <Form initialData={data} onSubmit={onClose}>
+      <Form.Field>
         <Form.Label>Name</Form.Label>
         <Form.Input placeholder="name" name="name"></Form.Input>
-        <Form.FieldGroup>
-          <Form.Button submit color={ButtonColor.LINK}>
-            Save
-          </Form.Button>
-          <Form.Button color={ButtonColor.LINK_LIGHT} onClick={onCancel}>
-            Cancel
-          </Form.Button>
-        </Form.FieldGroup>
       </Form.Field>
+      <Form.FieldGroup>
+        <Form.Button submit color={ButtonColor.LINK}>
+          Save
+        </Form.Button>
+        <Form.Button color={ButtonColor.LINK_LIGHT} onClick={onCancel}>
+          Cancel
+        </Form.Button>
+      </Form.FieldGroup>
     </Form>
   );
 }
