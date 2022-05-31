1d23b22db2440ca64af8f1792786f90a40454956
Krzysztof Grudziński <95306698+kgrudzinski@users.noreply.github.com>
2022-05-31 19:52:37 +0200

    fix: correct name of invalidated query

--- src/hooks/entries.js
+++ src/hooks/entries.js
@@ -15,7 +15,7 @@ export function useEntryMutations(onSuccess, onError) {
   const queryClient = useQueryClient();
 
   const on_success = () => {
-    queryClient.invalidateQueries(["Entries"]);
+    queryClient.invalidateQueries(["entries"]);
     onSuccess();
   };
 
