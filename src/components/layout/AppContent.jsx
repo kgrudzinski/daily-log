2a5472877a622f839c03fb3391419e477f3f7469
Krzysztof Grudziński <95306698+kgrudzinski@users.noreply.github.com>
2022-11-08 17:47:46 +0100

    fix: display red border around page content only in development mode

--- src/components/layout/AppContent.jsx
+++ src/components/layout/AppContent.jsx
@@ -1,9 +1,11 @@
+const style =
+  process.env.NODE_ENV === "development"
+    ? { border: "1px solid red", height: "100%" }
+    : { height: "100%" };
+
 export function AppContent({ children }) {
   return (
-    <div
-      className="content py-2 mr-2"
-      style={{ border: "1px solid red", height: "100%" }}
-    >
+    <div className="content py-2 mr-2" style={style}>
       {children}
     </div>
   );
