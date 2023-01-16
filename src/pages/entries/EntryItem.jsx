6a38aee11121c813a25334f73e5a87427186f2c2
Krzysztof Grudziński <95306698+kgrudzinski@users.noreply.github.com>
2023-01-16 19:30:13 +0100

    feat: change styling of EntryItem in day view
     - Height no longer depends on entrys duration
    - Changed positoion and font of title and duration fields
    
    Closes #7

--- src/pages/entries/EntryItem.jsx
+++ src/pages/entries/EntryItem.jsx
@@ -3,23 +3,18 @@ import { DateService } from "services";
 import "./entries.scss";
 
 export function EntryItem({ item }) {
-  const height = (item.duration / 5) * 10;
-  const itemStyle = { height: `${height}px` };
-
   const { editEntry } = useEntryContext();
   const time = DateService.formatTime(item.duration);
 
   return (
-    <div
-      style={itemStyle}
-      className="entry-item"
-      onClick={() => editEntry(item)}
-    >
-      <div>{item.task}</div>
-      <div>{item.description}</div>
-      <div>
-        {time.h}:{time.m}
+    <div className="entry-item" onClick={() => editEntry(item)}>
+      <div className="entry-item-header">
+        <span className="title is-5">{item.task}</span>
+        <span className="title is-5">
+          {time.h}:{time.m}
+        </span>
       </div>
+      <div>{item.description}</div>
     </div>
   );
 }
