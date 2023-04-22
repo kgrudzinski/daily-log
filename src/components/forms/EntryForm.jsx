8336bead1e952902bc6b7c045df7f3702c850215
Krzysztof Grudziński <95306698+kgrudzinski@users.noreply.github.com>
2023-04-22 13:59:05 +0200

    feat: change task selection in entry edit form.
    Simple select is replaced by an Autocomplete control. Tasks can be searched by
    their name or project name. By default completed tasks are not present
    in Autocomplete hint list. To make them appear in search results,
    one must check "show completed tasks" checkbox.
    
    Closes #16

--- src/components/forms/EntryForm.jsx
+++ src/components/forms/EntryForm.jsx
@@ -1,8 +1,11 @@
+import { useState } from "react";
 import { Form, ButtonColor } from "components/shared";
 import { useTasks } from "hooks";
+import { Status } from "consts";
 
 export function EntryForm({ data, onClose, onCancel }) {
   const { data: tasks, isLoading, isError, error } = useTasks();
+  const [showCompleted, setShowCompleted] = useState(false);
 
   if (data.taskId === 0) {
     data.taskId = tasks && tasks.length > 0 ? tasks[0].id : 0;
@@ -16,15 +19,35 @@ export function EntryForm({ data, onClose, onCancel }) {
     return <p>{error.message}</p>;
   }
 
+  const filteredItems = showCompleted
+    ? tasks
+    : tasks.filter((t) => t.status !== Status.COMPLETED);
+
+  const taskFilterFunc = (val, it) => {
+    const name = it.name.toLowerCase().indexOf(val.toLowerCase()) > -1;
+    const project =
+      it.projectName.toLowerCase().indexOf(val.toLowerCase()) > -1;
+    return name || project;
+  };
+
   return (
     <Form initialData={data} onSubmit={onClose}>
       <Form.Field>
         <Form.Label>Task</Form.Label>
-        <Form.Select name="taskId" expanded={1}>
-          {tasks.map((it) => {
-            return <Form.Option key={it.id} value={it.id} name={it.name} />;
-          })}
-        </Form.Select>
+        <Form.Autocomplete
+          name="taskId"
+          items={filteredItems}
+          filterFunc={taskFilterFunc}
+          renderListItem={(it) => <TaskListItem item={it} />}
+        />
+      </Form.Field>
+      <Form.Field>
+        <Form.Checkbox
+          value={showCompleted}
+          onChange={(evt) => setShowCompleted(evt.target.checked)}
+        >
+          Show completed tasks
+        </Form.Checkbox>
       </Form.Field>
       <Form.Field>
         <Form.Label>Description</Form.Label>
@@ -47,5 +70,20 @@ export function EntryForm({ data, onClose, onCancel }) {
         </Form.Button>
       </Form.FieldGroup>
     </Form>
+  );
+}
+
+function TaskListItem({ item }) {
+  return (
+    <>
+      <div className="has-text-weight-bold is-size-7">
+        <span className="mx-1">{item.projectName}</span>
+        <span>{">"}</span>
+        <span className="mx-1">{item.categoryName}</span>
+        <span>{">"}</span>
+        <span className="mx-1">{item.status}</span>
+      </div>
+      <div>{item.name}</div>
+    </>
   );
 }
