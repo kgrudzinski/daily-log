kgrudzinski/daily-log

commit 4eaf77b2a5d84d40db8f61f5ca05563385279c36

feat: close autocomplete hint list when component looses focus

src/components/shared/Autocomplete.jsx
@@ -54,6 +54,7 @@ export function Autocomplete({
 
   return (
     <div
+      onBlur={() => setExpanded(false)}
       className={
         expanded && filteredItems.length > 0 ? expanded_cls : hidden_cls
       }

commit a8ed35878e0951deb8fcd4718612e509eaaccf3c

fix: select autocomplete option
Clickin on autocomplete option did not select it.
Problem introduced in 4eaf77b2a5d84d40db8f61f5ca05563385279c36
and was caused by closing options list when main Autocomplete
div lost focus. Now list is closed if new fouced element (if any)
is not options list.

Fixes #18

src/components/shared/Autocomplete.jsx
@@ -54,7 +54,12 @@ export function Autocomplete({
 
   return (
     <div
-      onBlur={() => setExpanded(false)}
+      onBlur={(evt) => {
+        const focused = evt.relatedTarget;
+        if (!focused || !focused.classList.contains("dropdown-menu")) {
+          setExpanded(false);
+        }
+      }}
       className={
         expanded && filteredItems.length > 0 ? expanded_cls : hidden_cls
       }
@@ -81,7 +86,7 @@ export function Autocomplete({
           </div>
         </div>
       </div>
-      <div className="dropdown-menu" style={style}>
+      <div className="dropdown-menu" style={style} tabIndex={0}>
         <div className="dropdown-content">
           {filteredItems.map((item, index) => {
             return (